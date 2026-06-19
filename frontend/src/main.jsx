a730ae726a9317d5da62eb48953d416cdeff59be
amri-03 <amanrkumar030@gmail.com>
2026-06-19 11:25:09 +0530

    fix(frontend): update main.jsx to import and render Home directly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 
-import App from "./App";
+import Home from "./pages/Home";
 
 ReactDOM.createRoot (
     document.getElementById ( "root" )
 ).render (
     <React.StrictMode>
-        <App/>
+        <Home/>
     </React.StrictMode>
 );
